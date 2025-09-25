000f225be33a7f4dca3dceee9450e6ab2990d783
Dinesh-Senthil1507 <dineshsrn06@gmail.com>
2025-09-25 14:49:58 +0530

    second commit

--- 2.js
+++ 2.js
@@ -1,3 +1,21 @@
+let num="90";
+let a = Number(num);
+switch(true){
+    case ( a>=90 && a<=100):
+    console.log("A");
+    break;
+    case(a>=75 && a<=89): 
+    console.log("B");
+    break;
+    case(a>=50 && a<=74): 
+    console.log("C");
+    break;
+    default: 
+    console.log("Fail");
+    break;
+    }
+
+
 let num="3";
 let a=Number(num);
 if(a%2==0){
@@ -5,6 +23,8 @@ if(a%2==0){
 } else {
     console.log("Odd");
 }
+
+
 let s="3";
 let d=Number(s);
 switch(true){
